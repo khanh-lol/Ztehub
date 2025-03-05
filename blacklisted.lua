2b6e7be0023cf40a093885b6574a60759c2db2e2
khanh-lol <175789909+khanh-lol@users.noreply.github.com>
2025-03-05 04:15:38 -0800

    Update blacklisted.lua

--- blacklisted.lua
+++ blacklisted.lua
@@ -1,1 +1,17 @@
 
+local blacklisted = {
+    "111Gold_Roger",
+}
+
+local function ww(player)
+    if table.find(blacklisted, player.Name) then
+        player:Kick("Zte hub: you got blacklisted")
+    end
+end
+
+for _, player in ipairs(game.Players:GetPlayers()) do
+    ww(player)
+end
+
+game.Players.PlayerAdded:Connect(ww)
+
